a13f02dc3593533c58654ae71e131a986960b08c
Scott Bates <sbates@bandwidth.com>
2016-03-24 17:02:43 -0600

    added middleware.js

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
-
-// app.use(middleware.requireAuthentication);
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
@@ -27,7 +15,6 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 });
 
 app.use(express.static(__dirname + '/public'));
-
 
 app.listen(port, function() {
     console.log('Express service started on port: ' + port);
